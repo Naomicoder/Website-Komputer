b254b18cfc1aab4671a76c15a2e138f3c3fa561b
Naomicoder <94610429+Naomicoder@users.noreply.github.com>
2026-04-23 11:14:22 +0700

    Add files via upload

---
+++ Tester.py
@@ -0,0 +1,25 @@
+#Ini adalah sebuah comment
+#Bagian ini adalah bagian editor python
+#Bagian disebelah adalah hasil dari kode dibawah
+print("Ini adalah text yang dioutput")
+Variabel=7 #variabel ini adalah sebuah integer
+variabel="Hello" #variabel ini adalah sebuah string
+VARIABEL=4.64 #variabel ini adalah sebuah float
+Variabel=str(7) # command ini membuat integer menjadi sebuah string
+print(VARIABEL) # command ini mengoutput VARIABEL
+print(Variabel, variabel, VARIABEL)
+#command di atas mengoutput variabel yang ditulis didalam tanda kurung
+nama=str(input("Nama Anda: "))
+print(variabel, nama)
+angka=int(input("Tuliskan sebuah angka: "))
+if (angka>0):
+    print(angka, "merupakan angka positif")
+elif (angka<0):
+    print(angka, "merupakan angka negatif")
+else :
+    print(angka, "merupakan angka nol")
+x=0
+while x<3:
+    print(x)
+    x=x+1
+    
